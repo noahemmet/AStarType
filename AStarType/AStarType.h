e75ede8b0e7e95d127216323131efbe19002aa5d
Noah Emmet <noah@noahemmet.com>
2015-04-18 19:25:52 -0700

    Initial Commit

---
+++ AStarType/AStarType.h
@@ -0,0 +1,19 @@
+//
+//  AStarType.h
+//  AStarType
+//
+//  Created by Noah Emmet on 4/18/15.
+//  Copyright (c) 2015 Sticks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AStarType.
+FOUNDATION_EXPORT double AStarTypeVersionNumber;
+
+//! Project version string for AStarType.
+FOUNDATION_EXPORT const unsigned char AStarTypeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AStarType/PublicHeader.h>
+
+
